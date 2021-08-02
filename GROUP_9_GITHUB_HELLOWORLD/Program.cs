9266aa515bae3bf6c502cc11d3d2a7d1987027f8
Shader442 <keeganforsman@gmail.com>
2021-08-02 09:47:17 +0200

    Add project files.

---
+++ GROUP_9_GITHUB_HELLOWORLD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GROUP_9_GITHUB_HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
